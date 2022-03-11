b715cccabecec1c0237e6421dc43a0dc3081785b
Tertle950 <tertle950@gmail.com>
2022-03-11 16:11:12 +0000

    doing

--- thing.py
+++ thing.py
@@ -1,10 +1,23 @@
+#n → n/2 (n is even)
+#n → 3n + 1 (n is odd)
+
 collatzLen = {
-    1: 0
+    1: 1
 }
 
-def collatz(num){
+def collatz(num):
     if num in collatzLen:
         return collatzLen[num]
-    else
+    else:
+        if (num % 2) == 0:
+            #even
+            x = (num / 2)
+        else:
+            #odd
+            x = ((num * 3) + 1)
         
-}
+        print(x)
+        collatzLen[x] = collatz(x) + 1
+        return collatzLen[num]
+
+print(collatz(100))
